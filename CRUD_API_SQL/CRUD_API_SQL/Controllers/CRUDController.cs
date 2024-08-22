a843061113bb4151902e229c0d78121b2d7ed913
Sachin9010 <89120385+Sachin9010@users.noreply.github.com>
2024-08-22 17:28:29 +0530

    Get by Id and Post Method have been added

--- CRUD_API_SQL/CRUD_API_SQL/Controllers/CRUDController.cs
+++ CRUD_API_SQL/CRUD_API_SQL/Controllers/CRUDController.cs
@@ -1,7 +1,9 @@
 ﻿using CRUD_API_SQL.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Data.SqlClient;
+using System.Text;
 
 namespace CRUD_API_SQL.Controllers
 {
@@ -15,11 +17,76 @@ namespace CRUD_API_SQL.Controllers
         }
         [HttpGet]
 
-        public async Task<ActionResult<Students>> GetStudent() {
+        public string GetStudent() {
             SqlConnection con= new SqlConnection(_configuration.GetConnectionString("CRUDSQL").ToString());
             con.Open();
-            SqlCommand cmd = new SqlCommand("Select * from Students", con);
-            return Content("ok");
+            SqlDataAdapter cmd = new SqlDataAdapter("Select * from Student", con);
+            con.Close();
+            DataTable dt= new DataTable();
+            cmd.Fill(dt);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            foreach (DataRow row in dt.Rows)
+            {
+                sb.Append("{");
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    if (i > 0) { 
+                        sb.Append(",");
+                    }
+                    sb.AppendFormat("\"{0}\":\"{1}\"", dt.Columns[i].ColumnName, row[i].ToString().Replace("\"","\\\""));
+                }
+                sb.Append("},");
+            }
+            if (sb.Length > 0) {
+                sb.Length--;
+            }
+            sb.Append(']');
+            string jsonResult=sb.ToString();
+
+            return  jsonResult;
+        }
+
+        [HttpGet("{id}")]
+
+        public string GetStudentDetailsById(int id) {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("CRUDSQL").ToString());
+            con.Open();
+            SqlDataAdapter cmd = new SqlDataAdapter("Select * from Student where id="+id, con);
+            con.Close();
+            DataTable dt= new DataTable();
+            cmd.Fill(dt);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            foreach (DataRow row in dt.Rows)
+            {
+                sb.Append("{");
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+
+                    sb.AppendFormat("\"{0}\":\"{1}\"", dt.Columns[i].ColumnName, row[i].ToString().Replace("\"", "\\\""));
+                }
+                sb.Append("}]");
+            }
+        
+            return sb.ToString();
+        }
+
+        [HttpPost]
+        public string InsertStudentDetails(Student student)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("CRUDSQL").ToString());
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Student VALUES('"+student.Name+"','"+student.Class+"','"+student.FatherName+"','"+student.Subject+"','"+student.BirthYear+"')",con);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            
+            if (i > 0) {
+                return "Successfully inserted";
+            }
+            else
+            return "Error while inserting the record";
+
         }
 
     
