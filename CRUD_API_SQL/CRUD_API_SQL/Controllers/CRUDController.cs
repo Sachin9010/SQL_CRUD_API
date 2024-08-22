0236f0e89862ff8b45c502a85bca0eca4b015fdb
Sachin9010 <89120385+Sachin9010@users.noreply.github.com>
2024-08-22 11:16:04 +0530

    Add files via upload
    Day 1 Code

---
+++ CRUD_API_SQL/CRUD_API_SQL/Controllers/CRUDController.cs
@@ -0,0 +1,27 @@
+﻿using CRUD_API_SQL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CRUD_API_SQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CRUDController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public CRUDController(IConfiguration configuration) { 
+            _configuration = configuration;
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<Students>> GetStudent() {
+            SqlConnection con= new SqlConnection(_configuration.GetConnectionString("CRUDSQL").ToString());
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from Students", con);
+            return Content("ok");
+        }
+
+    
+    }
+}
---
+++ CRUD_API_SQL/CRUD_API_SQL/Models/Students.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUD_API_SQL.Models
+{
+    public class Students
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string FatherName { get; set; }
+        public string Subject { get; set; }
+        public string BirthYear { get; set; }
+    }
+}
